b5503265fcbaae3e401831d76b52dc87bd9bdeec
Petersjonny <pedrojulio.barreto@gmail.com>
2026-05-11 20:48:23 -0300

    Voltando tudo ao começo

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { Engine } from './core/Engine.js'; //Importa o motor
-
-// Garante que o DOM está pronto antes de iniciar
-window.addEventListener('load', () => {
-    const game = new Engine('gameCanvas'); //Instancia o jogo
-    game.init().catch(err => console.error("Erro no Vazio:", err)); //Inicia
-});
--- vite.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: './', // Isso garante que os caminhos do build fiquem relativos
-});
